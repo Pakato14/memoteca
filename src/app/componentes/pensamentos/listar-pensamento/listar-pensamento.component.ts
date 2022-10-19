e1806178a4c3658254680540a2d50edc7179a91a
Daniel Araújo <danieloliveira14@gmail.com>
2022-10-19 13:17:18 -0300

    ngFOR

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+   // {
+   //   conteudo: 'Minha propriedade é decorada com @Input()',
+   //   autoria: 'Componente Filho',
+   //   modelo: 'modelo1'
+   // },
+   // {
+   //   conteudo: 'Passo informações para o componente filho!',
+   //   autoria: 'Componente Pai',
+   //   modelo: 'modelo2'
+   // }
+  ]
   constructor() { }
 
   ngOnInit(): void {
